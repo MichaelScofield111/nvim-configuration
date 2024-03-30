22830bf551868c0af12b84d8b91c8cbfaee2f029
乾宝 <qianbao@laptop.local>
2024-03-30 16:31:42 +0800

    nvim-configuration

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("core.options")
+require("core.keymaps")
+
+
+-- plugins
+require("plugins.plugins-setup")
+require("plugins.theme")
+require("plugins.lualine")
+require("plugins.nvim-tree")
+require("plugins.treesitter")
+require("plugins.coc")
+require("plugins.comment")
---
+++ lua/plugins/plugins-setup.lua
@@ -0,0 +1,52 @@
+-- 自动安装packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- 保存此文件自动更新安装软件
+-- 注意PackerCompile改成了PackerSync
+-- plugins.lua改成了plugins-setup.lua，适应本地文件名字
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use {
+    'nvim-lualine/lualine.nvim',  -- 状态栏
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }  -- 状态栏图标
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',  -- 文档树
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- 文档树图标
+    }
+  }
+  use "christoomey/vim-tmux-navigator" -- 用ctl-hjkl来定位窗口
+  use "nvim-treesitter/nvim-treesitter" -- 语法高亮
+  use "p00f/nvim-ts-rainbow" -- 配合treesitter，不同括号颜色区分
+  
+  --  自动补全代码
+  use {'neoclide/coc.nvim', branch = 'release'}
+  
+  use "numToStr/Comment.nvim" -- gcc和gc注释
+
+
+
+if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
